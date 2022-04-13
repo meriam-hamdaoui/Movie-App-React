0042b5739256df8bf2d53a45b827af2dbba5d9ea
meriam <hamdaoui.meriam85@gmail.com>
2022-04-13 15:32:30 +0200

    final project

---
+++ src/Components/movieCards/MovieCard.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Rating from "../../Rating";
+import "./movieCard.css";
+
+const MovieCard = ({
+  id,
+  deleteFunction,
+  movieInfo: { title, year, description, posterURL, rating, trailerURL },
+}) => {
+  return (
+    <div className="movieCard">
+      <div className="cardHeader">
+        <h3 className="title">
+          {title}
+          <br /> <span>released</span> {year}
+        </h3>
+        <button onClick={() => deleteFunction(id)} className="btnX">
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
+      <div className="poster">
+        <img src={posterURL} alt={title} />
+      </div>
+      <div className="description">
+        <p>{description}</p>
+      </div>
+      <div className="cardBottom">
+        <a href={trailerURL} target="_blank" rel="noopener noreferrer">
+          WATCH TRAILER ►
+        </a>
+        <Rating rateNbr={rating} />
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
